aa85b225b34bf8ceb1208fa915663b1d713e9767
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-09-22 22:14:30 -0500

    updated code formating in Visual Studio and reformated sample file.

--- Assets/SampleScript.cs
+++ Assets/SampleScript.cs
@@ -2,20 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SampleScript : MonoBehaviour
-{
+public class SampleScript : MonoBehaviour {
     public int numberToDisplay;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         SayHelloWorld();
     }
 
-    private void SayHelloWorld()
-    {
-        for (int i = 0; i < numberToDisplay; i++)
-        {
+    private void SayHelloWorld() {
+        for (int i = 0; i < numberToDisplay; i++) {
             Debug.Log("Hello World");
         }
     }
