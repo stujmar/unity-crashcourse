f1e05374be1cee80fff3e8cff2d93fbb8a54fadd
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-09-22 22:33:45 -0500

    got a method to multiply numbers to the console

--- Assets/SampleScript.cs
+++ Assets/SampleScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class SampleScript : MonoBehaviour {
     public int numberToDisplay;
+    private int multiplier = 2;
 
     // Start is called before the first frame update
     void Start() {
         SayHelloWorld();
+        MultiplyNumber(5);
     }
 
     private void SayHelloWorld() {
         for (int i = 0; i < numberToDisplay; i++) {
             Debug.Log("Hello World");
         }
+    }
+
+    private void MultiplyNumber(int numberToMultiply) {
+        int product = numberToMultiply * multiplier;
+        Debug.Log(product);
     }
 }
