25e525ff263539be6a05c7e150531e527dc4e7c9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-09-22 22:04:33 -0500

    Made hello world script and put it on an object then ran the program

--- Assets/SampleScript.cs
+++ Assets/SampleScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SampleScript : MonoBehaviour
 {
+    public int numberToDisplay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SayHelloWorld();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SayHelloWorld()
     {
-        
+        for (int i = 0; i < numberToDisplay; i++)
+        {
+            Debug.Log("Hello World");
+        }
     }
 }
