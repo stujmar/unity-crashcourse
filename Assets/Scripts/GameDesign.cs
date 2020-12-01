6cbe9203813741a1e58aeec0e9bf353e20875229
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-01 17:42:08 -0600

    write mission in the game design document

--- Assets/Scripts/GameDesign.cs
+++ Assets/Scripts/GameDesign.cs
@@ -7,7 +7,18 @@ public class GameDesign : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      /*  Mission 
+        This is an isometric interior design puzzle game.
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         */
     }
 
     // Update is called once per frame
