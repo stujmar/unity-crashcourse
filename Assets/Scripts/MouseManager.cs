352d63eceac834ed977ad0e0d37ba800f69eb13f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-02 15:33:44 -0600

    Wrote Cursors Script

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    // Know what objects are clickable.
+    public LayerMask clickableLayer;
+
+    // Swap cursors for different objects.
+    public Texture2D pointer; // Normal pointer.
+    public Texture2D target; // Clickable pointer.
+    public Texture2D doorway; // Doorway pointer.
+    public Texture2D combat; // Combat pointer.
+
+    // Start is called before the first frame update
+    /* void Start()
+    {
+        
+    } */
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            if(hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            } else
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+        } else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
